d8c4fc39b7e54c9491e304470a14248ec3b0dbfb
TAKI <n_taki@taki-lab.site>
2018-07-14 20:29:18 +0900

    create window

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -1,11 +1,33 @@
+import java.awt.BorderLayout;
+import java.awt.Container;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
 
 public class MainWindow{
+    private String[] columnNames = {"品目", "タンパク質", "炭水化物", "脂質", "カロリー"};
+    private String[][] tabledata = {{"","","","",""}};
+
     public MainWindow(){
+        CreateWindow();
+    }
+
+    private void CreateWindow(){
         JFrame frame = new JFrame();
         frame.setSize(640, 480);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container contentPane = frame.getContentPane();
+        JButton addButton = new JButton("追加");
+        contentPane.add(addButton, BorderLayout.CENTER);
+
+        JTable listTable = new JTable(tabledata, columnNames);
+        JScrollPane sp = new JScrollPane(listTable);
+        contentPane.add(sp, BorderLayout.SOUTH);
+
         frame.setVisible(true);
     }
 }
