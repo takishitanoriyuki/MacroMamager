83e3b2d03c1502bc23acaf7aff4cadf1b8c3415e
TAKI <n_taki@taki-lab.site>
2018-09-11 10:27:20 +0900

    BasicDataクラスをシングルトン化

--- src/main/java/IBasicData.java
+++ src/main/java/IBasicData.java
@@ -4,23 +4,28 @@ public interface IBasicData{
      * @return
      */
     double getProtein();
+    void setProtein(double value);
     /**
      * カロリー
      */
     double getCalorie();
+    void setCalorie(double value);
     /**
      * 脂質
      * @return
      */
     double getLipid();
+    void setLipid(double value);
     /**
      * 炭水化物
      * @return
      */
     double getCarbohydrate();
+    void setCarbohydrate(double value);
     /**
      * ファイルを読み込めたかどうか
      * @return
      */
     boolean isExist();
+    boolean Save();
 }
